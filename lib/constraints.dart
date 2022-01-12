065b4b532e86a505165ff68a650325b831efa3ba
Justin-roy <roysunny951@gmail.com>
2022-01-12 12:37:03 +0530

    Done WIth Front Page

---
+++ lib/constraints.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  var txt = const TextStyle(
+    color: Colors.red,
+    fontSize: 38,
+    fontWeight: FontWeight.bold,
+  );
+  var txt2 = const TextStyle(
+    color: Colors.grey,
+    fontSize: 25,
+  );
+  var txt3 = const TextStyle(
+    color: Colors.grey,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+}
--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -1,44 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:gfgapp/constraints.dart';
 
-class profilepage extends StatelessWidget {
-  const profilepage({Key? key}) : super(key: key);
+class Profilepage extends StatelessWidget {
+  const Profilepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          "Netflix".toUpperCase(),
+          style: CustomTextStyle().txt,
+        ),
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 18.0),
-            child: Icon(
-              Icons.edit,
-              size: 30,
+          Container(
+            margin: const EdgeInsets.only(right: 8),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.edit),
             ),
           )
         ],
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        title: Text(
-          "NETFLIX",
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 35,
-          ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Center(
+              child: Text(
+                'Who\'s Watching ?',
+                style: CustomTextStyle().txt2,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                netflixAccountImg(1, 'Doremon'),
+                netflixAccountImg(2, 'Nobita'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                netflixAccountImg(3, 'Shizuka'),
+                netflixAccountImg(4, 'Suneo'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                netflixAccountImg(5, 'Dorami'),
+                netflixAccountImg(6, 'Gian'),
+              ],
+            ),
+          ],
         ),
       ),
-      backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100.0, left: 145),
-            child: Text(
-              "Who's Watching",
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-          )
-        ],
-      ),
+    );
+  }
+
+  Widget netflixAccountImg(int ac, String name) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image(
+            height: 120,
+            width: 120,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/netflixaccimg$ac.jpg'),
+          ),
+        ),
+        netflixAccName(name),
+      ],
+    );
+  }
+
+  Widget netflixAccName(String name) {
+    return Text(
+      name,
+      style: CustomTextStyle().txt3,
     );
   }
 }
