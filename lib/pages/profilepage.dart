14df615aa67c5c5dde3726f28570864bdfb385a9
kabirsinghcodes <kabirsingh0405@gmail.com>
2022-01-12 19:44:38 +0530

    profile page completed

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gfgapp/pages/constraints.dart';
 
 class profilepage extends StatelessWidget {
   const profilepage({Key? key}) : super(key: key);
@@ -28,17 +29,81 @@ class profilepage extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100.0, left: 145),
-            child: Text(
-              "Who's Watching",
-              style: TextStyle(color: Colors.grey, fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Text(
+                "Who's Watching?",
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 90.0),
+                  child: netflixAccountImg(1, 'KSC'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: netflixAccountImg(2, 'Nobita'),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 90.0),
+                  child: netflixAccountImg(3, 'Shizuka'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: netflixAccountImg(4, 'Suneo'),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 90.0),
+              child: Row(
+                children: [
+                  netflixAccountImg(5, 'Dorami'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget netflixAccountImg(int ac, String name) {
+  return Column(
+    children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(7),
+        child: Image(
+          height: 110,
+          width: 110,
+          fit: BoxFit.fill,
+          image: AssetImage('assets/images/netflixaccimg$ac.jpg'),
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      netflixAccName(name),
+    ],
+  );
+}
+
+Widget netflixAccName(String name) {
+  return Text(
+    name,
+    style: CustomTextStyle().txt3,
+  );
 }
